95bce0c93365a4194e7aa9f22fc7eb474d013ede
Calex8012 <42974499+Calex8012@users.noreply.github.com>
2019-06-18 14:31:38 -0500

    obo

---
+++ asesorias1.c
@@ -0,0 +1,21 @@
+#include <18F4620.h>
+#fuses HS, NOFCMEN, NOIESO, PUT, NOBROWNOUT, NOWDT
+#fuses NOPBADEN, NOMCLR, STVREN, NOLVP, NODEBUG
+#use delay(clock=16000000)
+
+#define __DEBUG_SERIAL__ //Si comentas esta linea se deshabilita el debug por serial y el PIN_C6 puede ser usado en forma digital I/O
+
+#ifdef __DEBUG_SERIAL__
+   #define TX_232        PIN_C6
+   #use RS232(BAUD=9600, XMIT=TX_232, BITS=8,PARITY=N, STOP=1)
+   #use fast_io(c)
+#endif
+
+//ISR_interrupcion()
+
+void main (void){
+   setup_oscillator(OSC_16MHZ);
+   while(1){
+      
+   }
+}
